fb8410e7146464fa181980917905f4a35f213295
CypressWriter <cypresswriter@gmail.com>
2022-04-21 10:10:52 -0700

    grid is now set up

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let length = 3;
+let length = 16;
 
 container = document.querySelector("div");
 
 for (let i = 0; i < length; i++) {
+    let square;
     for (let j = 0; j < length; j++) {
-        let square = document.createElement("div");
+        square = document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
     }
